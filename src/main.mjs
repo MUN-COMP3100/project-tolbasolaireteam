4e29121cc8eda98e3e45710c02d53e7a9574a863
Drew Griffiths <djamesg@mun.ca>
2023-02-18 14:02:23 -0330

    add user and password hashing, but what a mess!!

--- src/main.mjs
+++ src/main.mjs
@@ -1,20 +1,84 @@
 import { readFileSync } from 'fs';
 import * as mongo from './mongo.mjs';
+import { USERS } from './users.mjs';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 
+// let ingredient = 'beef'
 
-let ingredient = 'beef'
+// // return a list of recipes that contain the ingredient beef and store in a variable named arr
+// let arr = await mongo.findRecipeByIngredient(mongo.CLIENT, ingredient, 3);
+// console.log(arr);
 
-// return a list of recipes that contain the ingredient beef and store in a variable named arr
-let arr = await mongo.findRecipeByIngredient(mongo.CLIENT, ingredient, 3);
-console.log(arr);
+// // call anonymous function
+// (async () => {
+//     let category = "side-dish"
+//     let arr = await mongo.findRecipeByCategory(mongo.CLIENT, category, 3);
+//     console.log(arr);
+// })();
 
-// call anonymous function
-(async () => {
-    let category = "side-dish"
-    let arr = await mongo.findRecipeByCategory(mongo.CLIENT, category, 3);
-    console.log(arr);
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.nat73ng.mongodb.net/recipesData?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoString, {useNewUrlParser: true})
+
+mongoose.connection.on("error", function(error) {
+  console.log(error)
+})
+
+mongoose.connection.on("open", function() {
+  console.log("Connected to MongoDB database.")
+})
+
+// create a new user using the schema from users.mjs
+// const newUser = new USERS({
+//     firstName: "John",
+//     lastName: "Doe",
+//     email: "123@gmail.com",
+//     username: "123",
+//     password: "123"
+// })
+
+// save the new user to the database using async/await
+async function saveUser() {
+    try {
+        await newUser.save();
+        console.log("User saved to database.");
+    } catch (error) {
+        console.log(error);
+    } finally {
+        mongoose.connection.close();
+    }
+}
+
+// saveUser();
+
+// find a user using mongoose by their username and change their password
+(async() => {
+    try {
+        let newUser = await USERS.findOne({username: "1234"}).exec();
+        // check if the user exists
+        if (newUser) {
+            await newUser.save();
+            console.log("User saved to database.");
+        } else {
+            console.log("User not found.");
+        }
+    } catch (error) {
+        console.log(error);
+    } finally {
+        mongoose.connection.close();
+    }
 })();
+
+
+
+
+
+
+
 
 
 // const file = readFileSync('./recipeList2.0.json', 'utf-8');
--- src/users.mjs
+++ src/users.mjs
@@ -12,23 +12,29 @@ const USER_SCHEMA = new mongoose.Schema({
 // an async/await mongoose pre middleware function with a try/catch block to salt and hash the password before saving the user to the database
 USER_SCHEMA.pre("save", async function(next) {
     try {
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(this.password, salt)
-        this.password = hashedPassword
-        next()
+        const user = this;
+        if (!user.isModified("password") || !user.isNew) { return next(); }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        console.log(hashedPassword);
+        user.password = hashedPassword;
+        next();
     } catch (error) {
-        next(error)
+        next(error);
     }
 })
 
 // an async/await mongoose method with a try/catch block to compare the user's password with the hashed password in the database
 USER_SCHEMA.methods.isValidPassword = async function(newPassword) {
     try {
-        return await bcrypt.compare(newPassword, this.password)
+        return await bcrypt.compare(newPassword, this.password);
     } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
     }
 }
+
+
+export const USERS = mongoose.model("users", USER_SCHEMA);
 
 
 
