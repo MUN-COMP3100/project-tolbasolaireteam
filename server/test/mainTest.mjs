94dfbc5a5272b8482753b68fc245c3d7018e88d2
Drew Griffiths <djamesg@mun.ca>
2023-03-17 16:35:39 -0230

    added type of http call

--- server/test/mainTest.mjs
+++ server/test/mainTest.mjs
@@ -7,7 +7,7 @@ import { assert } from 'console';
 const create = axios.create;
 
 var myurl = 'http://localhost:3500';           
-// Let's configure the base url
+
 const instance = create({
     baseURL: myurl,
     timeout: 5000, 
@@ -20,7 +20,7 @@ describe('Meal Planner App - Tests with Mocha', function()
     { 
         describe('Test the /register route to sign-up a new user to the web app', function()
         { // ########################################## TEST /register ##########################################
-            it('Fail 1. Try to register/sign-up user with invalid first name in object.\n' +
+            it('Fail 1. POST - Try to register/sign-up user with invalid first name in object.\n' +
             '           This test should fail with a response message of "Invalid fields."',
             async function() 
             {
@@ -34,7 +34,7 @@ describe('Meal Planner App - Tests with Mocha', function()
                 strictEqual(response.data.message, 'Invalid fields.');
             });
 
-            it('Fail 2. Try to register/sign-up user with an empty required field (the password) in object.\n' +
+            it('Fail 2. POST - Try to register/sign-up user with an empty required field (the password) in object.\n' +
             '           This test should fail with a response message of "First and last name, email, and password are required."', 
             async function() 
             {
@@ -47,7 +47,7 @@ describe('Meal Planner App - Tests with Mocha', function()
                 strictEqual(response.data.message, 'First and last name, email, and password are required.');
             });
 
-            it('Fail 3. Try to register/sign-up user with an email that is already registered.\n' +
+            it('Fail 3. POST - Try to register/sign-up user with an email that is already registered.\n' +
             '           This test should fail with a response message of "Email is already registered."',
             async function() 
             {
@@ -61,7 +61,7 @@ describe('Meal Planner App - Tests with Mocha', function()
                 strictEqual(response.data.message, 'Email is already registered.');
             });
 
-            it('Pass 1. Try to register/sign-up a new user.\n' +
+            it('Pass 1. POST - Try to register/sign-up a new user.\n' +
             '           This test should pass with a response message of "New user amilcars1@mun.ca created!"',
             async function() 
             {
@@ -84,7 +84,7 @@ describe('Meal Planner App - Tests with Mocha', function()
 
         describe('Test the /auth route to sign-in a user to the web app', function()
         {// ########################################## TEST /auth ##########################################
-            it('Fail 1. Try to sign-in a user with an empty required field (the email in this case).\n' +
+            it('Fail 1. POST - Try to sign-in a user with an empty required field (the email in this case).\n' +
             '           This test should fail with a response message of "Email and password are required."',
             async function()
             {
@@ -96,7 +96,7 @@ describe('Meal Planner App - Tests with Mocha', function()
                 strictEqual(response.data.message, 'Email and password are required.');
             });
 
-            it('Fail 2. Try to sign-in a user with an invalid email.\n' +
+            it('Fail 2. POST - Try to sign-in a user with an invalid email.\n' +
             '           This test should fail with a response message of "User not found."',
             async function()
             {
@@ -108,7 +108,7 @@ describe('Meal Planner App - Tests with Mocha', function()
                 strictEqual(response.data.message, 'User not found.');
             });
 
-            it('Fail 3. Try to sign-in a user with an invalid password.\n' +
+            it('Fail 3. POST - Try to sign-in a user with an invalid password.\n' +
             '           This test should fail with a response message of "Password does not match."',
             async function()
             {
@@ -120,7 +120,7 @@ describe('Meal Planner App - Tests with Mocha', function()
                 strictEqual(response.data.message, 'Password does not match.');
             });
 
-            it('Pass 1. Try to sign-in a user with a valid email and password.\n' +
+            it('Pass 1. POST - Try to sign-in a user with a valid email and password.\n' +
             '           This test should pass with a access token and an array of roles.',
             async function()
             {
@@ -279,21 +279,21 @@ describe('Meal Planner App - Tests with Mocha', function()
 
             describe('Test the /recipes/findByIngredients route to get recipes by ingredients', function()
             {// ########################################## TEST /recipes/findByIngredients ##########################################
-                it('Fail 1. Try to get recipes by ingredients with no ingredients.\n' +
+                it('Fail 1. GET - Try to get recipes by ingredients with no ingredients.\n' +
                 '           This test should fail with a response message of "Ingredients required."',
                 async function() {
                     let response = await instance.get('/recipes/findByIngredients');
                     strictEqual(response.data.message, 'Ingredients required.');
                 });
 
-                it('Fail 2. Try to get recipes by ingredients with an invalid ingredient.\n' +
+                it('Fail 2. GET - Try to get recipes by ingredients with an invalid ingredient.\n' +
                 '           This test should fail with a response message of "No recipes match the ingredients battery,plutonium."',
                 async function() {
                     let response = await instance.get('/recipes/findByIngredients?ingredients=battery&ingredients=plutonium');
                     strictEqual(response.data.message, 'No recipes match the ingredients battery,plutonium.');
                 });
 
-                it('Pass 1. Try to get recipes by ingredients with valid ingredients.\n' +
+                it('Pass 1. GET - Try to get recipes by ingredients with valid ingredients.\n' +
                 '           This test should pass with data in the response.data object.', 
                 async function() {
                     let response = await instance.get('/recipes/findByIngredients?ingredients=macaroni&ingredients=butter&ingredients=flour&ingredients=salt&ingredients=milk&ingredients=cheese');
@@ -303,21 +303,21 @@ describe('Meal Planner App - Tests with Mocha', function()
 
             describe('Test the /recipes/findByType route to get recipes by type', function()
             {// ########################################## TEST /recipes/findByType ##########################################
-                it('Fail 1. Try to get recipes by type with no type.\n' +
+                it('Fail 1. GET - Try to get recipes by type with no type.\n' +
                 '           This test should fail with a response message of "Type required."',
                 async function() {
                     let response = await instance.get('/recipes/findByType');
                     strictEqual(response.data.message, 'Type required.');
                 });
 
-                it('Fail 2. Try to get recipes by type with an invalid type.\n' +
+                it('Fail 2. GET - Try to get recipes by type with an invalid type.\n' +
                 '           This test should fail with a response message of "No recipes match the type invalid."',
                 async function() {
                     let response = await instance.get('/recipes/findByType?type=invalid');
                     strictEqual(response.data.message, 'No recipes match the type invalid.');
                 });
 
-                it('Pass 1. Try to get recipes by type with valid type.\n' +
+                it('Pass 1. GET - Try to get recipes by type with valid type.\n' +
                 '           This test should pass with data in the response.data object.', 
                 async function() {
                     let response = await instance.get('/recipes/findByType?type=main course');
